be1b5c2b87cfe22c5e0ca29fc917290a69678bf3
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-06-05 21:53:26 -0300

    PermanentDrawer implemented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from 'next/app';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
+import PermanentDrawerLeft from '../src/components/specific/PermanentDrawerLeft';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <CssBaseline />
-      <Component {...pageProps} />
+      <PermanentDrawerLeft>
+        <Component {...pageProps} />
+      </PermanentDrawerLeft>
     </>
   );
 }
